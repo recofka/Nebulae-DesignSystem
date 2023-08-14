3b4ac2fddb3d360fa9cb73c9721eff5ee0134164
Denise Recofka <recofka@gmail.com>
2023-08-14 16:28:08 +0200

    Add Storybook config to show docs page

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -19,13 +19,14 @@ const config = {
     getAbsolutePath("@storybook/addon-essentials"),
     getAbsolutePath("@storybook/addon-onboarding"),
     getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath("@storybook/addon-docs")
   ],
   framework: {
     name: getAbsolutePath("@storybook/react-vite"),
     options: {},
   },
   docs: {
-    autodocs: "tag",
+    autodocs: true,
   },
 };
 export default config;
